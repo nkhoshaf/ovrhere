d2f807f9a1e013210cc14e8fdb68ed8cb7442e54
Nareg Khoshafian <>
2018-11-12 12:07:01 -0500

    OvrHere Main Viecontroller Setup

--- ovrhere/AppDelegate.swift
+++ ovrhere/AppDelegate.swift
@@ -14,18 +14,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let rootVC = OvrHereViewController(nibName: "OvrHereViewController", bundle: nil)
+        if let window = window {
+            window.rootViewController = rootVC
+            window.makeKeyAndVisible()
+        }
         return true
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {}
-
-    func applicationDidEnterBackground(_ application: UIApplication) {}
-
-    func applicationWillEnterForeground(_ application: UIApplication) {}
-
-    func applicationDidBecomeActive(_ application: UIApplication) {}
-
-    func applicationWillTerminate(_ application: UIApplication) {}
-
 }
 
--- ovrhere/OvrHereViewController.swift
+++ ovrhere/OvrHereViewController.swift
@@ -12,9 +12,12 @@ class OvrHereViewController: UIViewController {
 
     let wordsDictionary = ["hello","world","yo","where","dog","sup"]
     
+    @IBOutlet weak var sup: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        sup.text = "yo"
     }
 
 }
